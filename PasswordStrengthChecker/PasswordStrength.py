4a2b1676bff5e84a11ad0a3389f58a8fd39fefe2
ADGusst <adgusst@gmail.com>
2025-12-22 21:36:09 -0500

    Updated password validation program
    Updated password validation program

--- PasswordStrengthChecker/PasswordStrength.py
+++ PasswordStrengthChecker/PasswordStrength.py
@@ -4,16 +4,67 @@
 #  Revision:  
 ###############################################################
 
-pw = input("Enter your password: ")
+# Importing necessary libraries
+import string
 
-print(pw)
+# Taking password input from the user.
+password = input("Enter your password: ")
 
-print(len(pw))
+# This functions checks the length of the password.
+def check_password_length(password):
+    if len(password) > 8 and len(password) < 16:
+        return 1
+    elif len(password) == 16 or len(password) > 16:
+        return 2
+    else:
+        return 0
+    
+# This function checks the strength of the password.
 
-if len(pw) > 8 and len(pw) < 16:
-    print("Your password is of moderate length")
-elif len(pw) > 16:
-    print("Your password is of good length")
-else:
-    print("Your password is to short")
+def check_password_strength(password):
+    if password.islower() == True:
+        return 0
+    elif password.isupper() == True:
+        return 0
+    elif password.isalpha() == True:
+        return 0
+    elif password.isdigit() == True:
+        return 0
+    else:
+        return 2
 
+
+# This function checks if the password contains any special characters.
+def check_special_chars(password):
+    #print(*[x for x in password if x in string.punctuation])
+    if len([x for x in password if x in string.punctuation]) > 0:
+        return 2
+    else:
+        return 0
+
+
+# This function runs all the tests and returns the final score.
+
+def run_test():
+    pass_len = check_password_length(password)
+    pass_special = check_special_chars(password)
+    pass_strength = check_password_strength(password)
+
+    check_score = pass_len + pass_special + pass_strength
+    print("Your password score is", check_score)
+    if check_score == 6:
+        print("Your password is very strong")
+    elif check_score == 5:
+        print("Your password is strong")
+    elif check_score == 4:
+        print("Your password is moderate")
+    elif check_score == 3:
+        print("Your password is weak")
+    elif check_score == 2:
+        print("Your password is very weak")
+    else:
+        print("You should use a password manager to generate a strong password")
+        print("Good password practices include using a mix of uppercase and lowercase letters, numbers, and special characters")
+
+
+run_test()
